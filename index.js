c3f9e85b11465233e3d8cd9d10a287c56022e059
Senapei <115939642+Senapei@users.noreply.github.com>
2023-03-02 15:29:26 +0000

    Add sounds to keyboard and mouse clicks

---
+++ index.js
@@ -0,0 +1,60 @@
+let tom1 = new Audio("./sounds/tom-1.mp3");
+let tom2 = new Audio("./sounds/tom-2.mp3");
+let tom3 = new Audio("./sounds/tom-3.mp3");
+let tom4 = new Audio("./sounds/tom-4.mp3");
+let snare = new Audio("./sounds/snare.mp3");
+let crash = new Audio("./sounds/crash.mp3");
+let kickBass = new Audio("./sounds/kick-bass.mp3");
+
+for (let i = 0; i < document.querySelectorAll(".drum-button").length; i++) {
+  document
+    .querySelectorAll(".drum-button")
+    [i].addEventListener("click", function () {
+      let buttonInnerHTML = this.innerHTML;
+      playingSound(buttonInnerHTML);
+      buttonAnimation(buttonInnerHTML);
+    });
+}
+
+addEventListener("keydown", (event) => {
+  playingSound(event.key);
+  buttonAnimation(event.key);
+});
+
+function playingSound(letter) {
+  letter = letter.toUpperCase();
+  switch (letter) {
+    case "W":
+      tom1.play();
+      break;
+    case "A":
+      tom2.play();
+      break;
+    case "S":
+      tom3.play();
+      break;
+    case "D":
+      tom4.play();
+      break;
+    case "J":
+      snare.play();
+      break;
+    case "K":
+      crash.play();
+      break;
+    case "L":
+      kickBass.play();
+      break;
+    default:
+      break;
+  }
+}
+
+function buttonAnimation(letter) {
+  letter = letter.toLowerCase();
+  let currentButton = document.querySelector("." + letter);
+  currentButton.classList.add("pressed");
+  setTimeout(function () {
+    currentButton.classList.remove("pressed", 100);
+  });
+}
